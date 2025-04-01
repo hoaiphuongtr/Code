21dd6b5e2b4127010a307dea78582b32fff94b08
hoaiphuongtr <ctbt2004@gmail.com>
2025-04-01 12:41:56 +0700

    Complete delete feature

--- ToDoList/my-app/src/components/TaskList/TaskList.tsx
+++ ToDoList/my-app/src/components/TaskList/TaskList.tsx
@@ -1,44 +1,56 @@
-import { ToDo } from '../../@types/todo.type'
-import styles from './taskList.module.scss'
+import { ToDo } from '../../@types/todo.type';
+import styles from './taskList.module.scss';
 
 interface TaskListProps {
-  doneTaskList?: boolean
-  todos: ToDo[]
-  handleToDo: (id: string, done: boolean) => void
-  startToDo: (id: string) => void
+    doneTaskList?: boolean;
+    todos: ToDo[];
+    handleToDo: (id: string, done: boolean) => void;
+    startToDo: (id: string) => void;
+    deleteToDo: (id: string) => void;
 }
 export default function TaskList(props: TaskListProps) {
-  const { doneTaskList, todos, handleToDo, startToDo } = props
-  const onChangeCheckbox = (idToDo: string) => (event: React.ChangeEvent<HTMLInputElement>) => {
-    handleToDo(idToDo, event.target.checked)
-  }
-  return (
-    <div className='mb-2'>
-      <h2 className={styles.title}>{doneTaskList ? 'Hoàn Thành' : 'Chưa hoàn thành'}</h2>
-      <div className={styles.tasks}>
-        {todos.map((todo) => (
-          <div className={styles.task} key={todo.id}>
-            <input
-              type='checkbox'
-              className={styles.taskCheckbox}
-              checked={todo.done}
-              onChange={onChangeCheckbox(todo.id)}
-            />
-            <span
-              className={`${styles.taskName}
+    const { doneTaskList, todos, handleToDo, startToDo, deleteToDo } = props;
+    const onChangeCheckbox =
+        (idToDo: string) => (event: React.ChangeEvent<HTMLInputElement>) => {
+            handleToDo(idToDo, event.target.checked);
+        };
+    return (
+        <div className='mb-2'>
+            <h2 className={styles.title}>
+                {doneTaskList ? 'Hoàn Thành' : 'Chưa hoàn thành'}
+            </h2>
+            <div className={styles.tasks}>
+                {todos.map((todo) => (
+                    <div className={styles.task} key={todo.id}>
+                        <input
+                            type='checkbox'
+                            className={styles.taskCheckbox}
+                            checked={todo.done}
+                            onChange={onChangeCheckbox(todo.id)}
+                        />
+                        <span
+                            className={`${styles.taskName}
                     ${todo.done ? styles.taskNameDone : ''}`}
-            >
-              {todo.name}
-            </span>
-            <div className={styles.taskAction}>
-              <button className={styles.taskBtn} onClick={() => startToDo(todo.id)}>
-                🖋️
-              </button>
-              <button className={styles.taskBtn}>🗑️</button>
+                        >
+                            {todo.name}
+                        </span>
+                        <div className={styles.taskAction}>
+                            <button
+                                className={styles.taskBtn}
+                                onClick={() => startToDo(todo.id)}
+                            >
+                                🖋️
+                            </button>
+                            <button
+                                className={styles.taskBtn}
+                                onClick={() => deleteToDo(todo.id)}
+                            >
+                                🗑️
+                            </button>
+                        </div>
+                    </div>
+                ))}
             </div>
-          </div>
-        ))}
-      </div>
-    </div>
-  )
+        </div>
+    );
 }
--- ToDoList/my-app/src/components/TodoList/TodoList.tsx
+++ ToDoList/my-app/src/components/TodoList/TodoList.tsx
@@ -1,65 +1,117 @@
-import TaskInput from '../TaskInput'
-import styles from './todoList.module.scss'
+import { useEffect, useState } from 'react';
+import TaskInput from '../TaskInput';
+import styles from './todoList.module.scss';
+import TaskList from '../TaskList';
+import { ToDo } from '../../@types/todo.type';
 
-import TaskList from '../TaskList'
-import { ToDo } from '../../@types/todo.type'
-import { useState } from 'react'
+// interface handleNewToDos {
+//     (toDoObj: ToDo[]): ToDo[];
+// }
+type handleNewToDos = (toDoObj: ToDo[]) => ToDo[];
+const syncReactToLocalStorage = (handleNewToDos: handleNewToDos) => {
+    const toDoString = localStorage.getItem('toDos');
+    const toDoObj: ToDo[] = toDoString ? JSON.parse(toDoString) : [];
+    const newToDoObj = handleNewToDos(toDoObj);
+    localStorage.setItem('toDos', JSON.stringify(newToDoObj));
+};
+
 export default function TodoList() {
-  const [toDos, setToDos] = useState<ToDo[]>([])
-  const [currentToDo, setCurrentToDo] = useState<ToDo | null>(null)
-  const addToDo = (name: string) => {
-    const newToDo: ToDo = {
-      name,
-      done: false,
-      id: new Date().toISOString()
-    }
-    setToDos((prevState) => [...prevState, newToDo])
-  }
-  const alreadyDoneTask = toDos.filter((toDo) => toDo.done)
-  const notYetTask = toDos.filter((toDo) => !toDo.done)
-  const handleToDo = (id: string, done: boolean) => {
-    setToDos((prevState) =>
-      prevState.map((toDo) => {
-        if (toDo.id === id) {
-          return { ...toDo, done }
+    const [toDos, setToDos] = useState<ToDo[]>([]);
+    const [currentToDo, setCurrentToDo] = useState<ToDo | null>(null);
+
+    useEffect(() => {
+        const toDoString = localStorage.getItem('toDos');
+        const toDoObj: ToDo[] = toDoString ? JSON.parse(toDoString) : [];
+        setToDos(toDoObj);
+    }, []);
+
+    const addToDo = (name: string) => {
+        const newToDo: ToDo = {
+            name,
+            done: false,
+            id: new Date().toISOString()
+        };
+        setToDos((prevState) => [...prevState, newToDo]);
+        syncReactToLocalStorage((toDoObj: ToDo[]) => [...toDoObj, newToDo]);
+    };
+    const alreadyDoneTask = toDos.filter((toDo) => toDo.done);
+    const notYetTask = toDos.filter((toDo) => !toDo.done);
+    const handleToDo = (id: string, done: boolean) => {
+        setToDos((prevState) =>
+            prevState.map((toDo) => {
+                if (toDo.id === id) {
+                    return { ...toDo, done };
+                }
+                return toDo;
+            })
+        );
+    };
+    const startToDo = (id: string) => {
+        const findedToDo = toDos.find((todo) => todo.id === id);
+        if (findedToDo) {
+            setCurrentToDo(findedToDo);
         }
-        return toDo
-      })
-    )
-  }
-  const startToDo = (id: string) => {
-    const findedToDo = toDos.find((todo) => todo.id === id)
-    if (findedToDo) {
-      setCurrentToDo(findedToDo)
-    }
-  }
-  const editToDo = (name: string) => {
-    setCurrentToDo((prevState) => {
-      if (prevState) return { ...prevState, name }
-      return null
-    })
-  }
-  const finishEditToDo = () => {
-    setToDos((prevState) =>
-      prevState.map((todo) => {
-        if (todo.id === (currentToDo as ToDo).id) {
-          return currentToDo as ToDo
+    };
+    const editToDo = (name: string) => {
+        setCurrentToDo((prevState) => {
+            if (prevState) return { ...prevState, name };
+            return null;
+        });
+    };
+    const finishEditToDo = () => {
+        const handler = (toDoObj: ToDo[]) =>
+            toDoObj.map((todo) => {
+                if (todo.id === (currentToDo as ToDo).id) {
+                    return currentToDo as ToDo;
+                }
+                //if (todo.id === currentToDo?.id){
+                // return currentToDo;
+                //}
+                return todo;
+            });
+        setToDos(handler);
+        setCurrentToDo(null);
+        syncReactToLocalStorage(handler);
+    };
+    const deleteToDo = (id: string) => {
+        if (currentToDo) {
+            setCurrentToDo(null);
         }
-        //if (todo.id === currentToDo?.id){
-        // return currentToDo;
-        //}
-        return todo
-      })
-    )
-    setCurrentToDo(null)
-  }
-  return (
-    <div className={styles.toDoList}>
-      <div className={styles.container}>
-        <TaskInput addToDo={addToDo} currentToDo={currentToDo} editToDo={editToDo} finishEditToDo={finishEditToDo} />
-        <TaskList todos={notYetTask} handleToDo={handleToDo} startToDo={startToDo} />
-        <TaskList doneTaskList todos={alreadyDoneTask} handleToDo={handleToDo} startToDo={startToDo} />
-      </div>
-    </div>
-  )
+        const handler = (toDoObj: ToDo[]) => {
+            const result = [...toDoObj];
+            const findedIndexToDo = toDoObj.findIndex((todo) => todo.id === id);
+            if (findedIndexToDo !== -1) {
+                result.splice(findedIndexToDo, 1);
+                return result;
+            }
+            return toDoObj;
+        };
+        setToDos(handler);
+        syncReactToLocalStorage(handler);
+    };
+    return (
+        <div className={styles.toDoList}>
+            <div className={styles.container}>
+                <TaskInput
+                    addToDo={addToDo}
+                    currentToDo={currentToDo}
+                    editToDo={editToDo}
+                    finishEditToDo={finishEditToDo}
+                />
+                <TaskList
+                    todos={notYetTask}
+                    handleToDo={handleToDo}
+                    startToDo={startToDo}
+                    deleteToDo={deleteToDo}
+                />
+                <TaskList
+                    doneTaskList
+                    todos={alreadyDoneTask}
+                    handleToDo={handleToDo}
+                    startToDo={startToDo}
+                    deleteToDo={deleteToDo}
+                />
+            </div>
+        </div>
+    );
 }
